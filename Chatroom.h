3324bd0aa36c44963fbbd13c3a3962dad67f8fb2
Jared <jaredluke3@gmail.com>
2025-09-25 19:56:37 +0200

    Add ChatRoom class definition with user vector

---
+++ Chatroom.h
@@ -0,0 +1,17 @@
+#ifndef CHATROOM_H
+#define CHATROOM_H
+
+#include <vector>
+using namespace std;
+class Users;
+
+class ChatRoom {
+
+    private:
+        vector<Users*> users;
+            
+    public:
+        
+};
+
+#endif
